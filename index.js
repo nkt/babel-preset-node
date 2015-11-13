1217b97322f513763d4ad0496769380c2fa2f8bc
mattstyles <matt@veryfizzyjelly.com>
2015-11-13 22:37:41 +0000

    #remove unnecessary transforms for node v4

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
     require('babel-plugin-transform-es2015-template-literals'),
     require('babel-plugin-transform-es2015-literals'),
     require('babel-plugin-transform-es2015-function-name'),
-    require('babel-plugin-transform-es2015-arrow-functions'),
     require('babel-plugin-transform-es2015-block-scoped-functions'),
     require('babel-plugin-transform-es2015-object-super'),
     require('babel-plugin-transform-es2015-shorthand-properties'),
@@ -13,10 +12,8 @@ module.exports = {
     require('babel-plugin-transform-es2015-sticky-regex'),
     require('babel-plugin-transform-es2015-unicode-regex'),
     require('babel-plugin-transform-es2015-constants'),
-    require('babel-plugin-transform-es2015-spread'),
     require('babel-plugin-transform-es2015-parameters'),
     require('babel-plugin-transform-es2015-destructuring'),
-    require('babel-plugin-transform-es2015-block-scoping'),
     require('babel-plugin-transform-es2015-modules-commonjs'),
     // ES7
     [require('babel-plugin-transform-async-to-module-method'), {
